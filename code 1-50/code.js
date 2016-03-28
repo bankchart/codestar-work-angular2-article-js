0d07d8f3b9c11f24debe268e14a2e02495f1e4e1
bankchart <mrbankshart@gmail.com>
2016-03-28 15:33:47 +0700

    coding... ex-10

---
+++ code 1-50/code.js
@@ -0,0 +1,135 @@
+/* start: ex-1 */
+function meterToFoot(x){
+    return 3.28084 * x;
+}
+document.getElementById('ex-code-1').innerHTML = '\nfunction meterToFoot(x){\n' +
+                                                    '\treturn 3.28084 * x;' +
+                                                '\n}';
+document.getElementById('ex-usage-1').innerHTML = '\nmeterToFoot(5);';
+document.getElementById('ex-output-1').innerHTML = '\n' + meterToFoot(5) + ' feet';
+/* end: ex-1 */
+
+/* start: ex-2 */
+function meterToYard(x){
+    return 1.09361 * x;
+}
+document.getElementById('ex-code-2').innerHTML = '\nfunction meterToYard(x){\n' +
+                                                    '\treturn 1.09361 * x;' +
+                                                '\n}';
+document.getElementById('ex-usage-2').innerHTML = '\nmeterToYard(5);';
+document.getElementById('ex-output-2').innerHTML = '\n' + meterToYard(5) + ' yards';
+/* end: ex-2 */
+
+/* start: ex-3 */
+function area(width, height){
+    return width * height;
+}
+document.getElementById('ex-code-3').innerHTML = '\nfunction area(width, height){\n' +
+                                                    '\treturn width * height;' +
+                                                '\n}';
+document.getElementById('ex-usage-3').innerHTML = '\narea(5, 10);';
+document.getElementById('ex-output-3').innerHTML = '\n' + area(5, 10) + ' m<sup>2</sup>';
+/* end: ex-3 */
+
+/* start: ex-4 */
+function interest(n){
+    return n * 0.0125;
+}
+document.getElementById('ex-code-4').innerHTML = '\nfunction interest(n){\n' +
+                                                    '\treturn n * 0.0125;' +
+                                                '\n}';
+document.getElementById('ex-usage-4').innerHTML = '\ninterest(15000);';
+document.getElementById('ex-output-4').innerHTML = '\n' + interest(15000) + ' บาท';
+/* end: ex-4 */
+
+/* start: ex-5 */
+function areaOfCircle(r){
+    return Math.PI * Math.pow(r, 2);
+}
+document.getElementById('ex-code-5').innerHTML = '\nfunction areaOfCircle(r){\n' +
+                                                    '\treturn Math.PI * Math.pow(r, 2);' +
+                                                '\n}';
+document.getElementById('ex-usage-5').innerHTML = '\nareaOfCircle(5);';
+document.getElementById('ex-output-5').innerHTML = '\n' + areaOfCircle(5) + ' m<sup>2</sup>';
+/* end: ex-5 */
+
+/* start: ex-6 */
+function thaiArea(a){
+    return a / 1600;
+}
+document.getElementById('ex-code-6').innerHTML = '\nfunction thaiArea(a){\n' +
+                                                    '\treturn a / 1600;' +
+                                                '\n}';
+document.getElementById('ex-usage-6').innerHTML = '\nthaiArea(10000);';
+document.getElementById('ex-output-6').innerHTML = '\n' + thaiArea(10000) + ' rai';
+/* end: ex-6 */
+
+/* start: ex-7 */
+/* ref: http://www.mathportal.org/calculators/trigonometry-calculators/degrees-to-radians-converter.php */
+function degreeToRadian(d){
+    var symbol_sign = d > 0 ? '' : '-';
+    var temp_1 = Math.abs(d);
+    var temp_2 = 180;
+    if(temp_1 == 0)
+        return '0\u03C0';
+    for(var i=temp_1-1;i>1;i--){
+        if(temp_1%i == 0 && temp_2%i == 0){
+            temp_1 /= i;
+            temp_2 /= i;
+        }
+    }
+    return temp_1 == temp_2 ?
+        symbol_sign + '\u03C0' : symbol_sign + (temp_1 == 1 ? '' : temp_1) + '\u03C0' +
+        (temp_2 == 1 ? '' : '/' + temp_2);
+}
+document.getElementById('ex-code-7').innerHTML = '\nfunction degreeToRadian(d){\n' +
+                                                        '\tvar symbol_sign = d > 0 ? \'\' : \'-\';\n' +
+                                                        '\tvar temp_1 = Math.abs(d);\n' +
+                                                        '\tvar temp_2 = 180;\n' +
+                                                        '\tif(temp_1 == 0)\n' +
+                                                            '\t\treturn \'0\\u03C0\';\n' +
+                                                        '\tfor(var i=temp_1-1;i>1;i--){\n' +
+                                                            '\t\tif(temp_1%i == 0 && temp_2%i == 0){\n' +
+                                                                '\t\t\ttemp_1 /= i;\n' +
+                                                                '\t\t\ttemp_2 /= i;\n' +
+                                                            '\t\t}\n' +
+                                                        '\t}\n' +
+                                                        '\treturn temp_1 == temp_2 ?\n' +
+                                                        '\t\'\\u03C0\' : (temp_1 == 1 ? \'\' : temp_1) + \'\\u03C0\' + \'/\' + temp_2;\n' +
+                                                    '}';
+document.getElementById('ex-usage-7').innerHTML = '\ndegreeToRadian(-30);';
+document.getElementById('ex-output-7').innerHTML = '\n' + degreeToRadian(-30);
+/* end: ex-7 */
+
+/* start: ex-8 */
+function ounceToTical(o){
+    /*
+        1000/35.274 = x/1
+        1000 กรัม = 35.274 ออนซ์
+        1 ออนซ์ = 28.349492544 กรัม
+        1 บาท = 15.2 กรัม
+        1 ออนซ์ = 1.865098194 บาท
+    */
+    return o * ((1000/35.274)/15.2);
+}
+document.getElementById('ex-code-8').innerHTML = '\nfunction ounceToTical(o){\n' +
+                                                    '\treturn o * ((1000/35.274)/15.2);' +
+                                                '\n}';
+document.getElementById('ex-usage-8').innerHTML = '\nounceToTical(50);';
+document.getElementById('ex-output-8').innerHTML = '\n' + ounceToTical(50) + ' บาท';
+/* end: ex-8 */
+
+/* start: ex-9 */
+function poundToGram(x){
+    return x * 453.59237;
+}
+document.getElementById('ex-code-9').innerHTML = '\nfunction poundToGram(x){\n' +
+                                                    '\treturn x * 453.59237;' +
+                                                '\n}';
+document.getElementById('ex-usage-9').innerHTML = '\npoundToGram(12);';
+document.getElementById('ex-output-9').innerHTML = '\n' + poundToGram(12) + ' กรัม';
+/* end: ex-9 */
+
+/* start: ex-10 */
+
+/* end: ex-10 */
